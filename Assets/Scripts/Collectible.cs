dd8c01b95275513862b1cb6f0c6b8a1650f7266c
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-03-13 00:12:00 +0530

    refactor: collectible script

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -1,11 +1,8 @@
 using UnityEngine;
 
-public class Collectible : MonoBehaviour
-{
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
+public class Collectible : MonoBehaviour {
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
             Destroy(gameObject);
         }
     }
